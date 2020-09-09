d73618768b31f662bf461ae9308637e874fcc574
naereloire <naere.loire@gmail.com>
2020-09-09 01:29:50 -0300

    add rotas login e home

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
-
-function App() {
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './pages/login/login';
+import Home from './pages/login/home/home';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello! </h1>
-      </header>{' '}
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Login />
+        </Route>
+        <Route path="/home">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
-}
-
+};
 export default App;
