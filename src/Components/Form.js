71cba28a729ff2fe419c81268bf4b44d85f9ca59
naereloire <naere.loire@gmail.com>
2020-09-09 02:23:03 -0300

    install uuid

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,5 +1,65 @@
 import React, { useState } from 'react';
 import firebase from '../config/fireconfig.js';
+import axios from axios;
+
+const ObjetoTest = {
+  "idempotencyKey": "1a6b0c3c-1a98-4ddc-8f10-5dad741a7ef3",
+  "properties": "sdfwerhrebgh",
+  "owner": {
+    "type": "PJ",
+    "name": "Jon Snow Corporative ",
+    "companyName": "TT",
+    "email": "jon@ppi.com.br",
+    "homePhone": "1131859600",
+    "businessPhone": "1131859600",
+    "mobilePhone": "1131859600",
+    "address": {
+      "street": "Winterfell",
+      "number": "529",
+      "district": "Winterfell",
+      "city": "Westeros",
+      "state": "SP",
+      "zipcode": "06460080"
+    },
+    "identifierDocument": {
+      "document": "00270965000102",
+      "type": "CNPJ"
+    },
+    "documents": [
+      {
+        "type": "CNPJ",
+        "document": "00270965000102"
+      }
+    ]
+  },
+  "responsible": {
+    "type": "PF",
+    "name": "Jon Snow",
+    "companyName": "TT",
+    "email": "jon@ppi.com.br",
+    "homePhone": "1131859600",
+    "businessPhone": "1131859600",
+    "mobilePhone": "1131859600",
+    "address": {
+      "street": "Winterfell",
+      "number": "529",
+      "district": "Winterfell",
+      "city": "Westeros",
+      "state": "SP",
+      "zipcode": "06460080"
+    },
+    "identifierDocument": {
+      "document": "76227631019",
+      "type": "CPF"
+    },
+    "documents": [
+      {
+        "type": "CPF",
+        "document": "76227631019"
+      }
+    ]
+  }
+}
 
 export const Form = () => {
   const [email, setEmail] = useState(''),
@@ -15,7 +75,10 @@ export const Form = () => {
     firebase
       .auth()
       .createUserWithEmailAndPassword(email, password)
-      .then(() => {});
+      .then(() => {
+        ObjetoTest.owner.email = email;
+        ObjetoTest.responsible.email = email;
+      });
   };
 
   return (
