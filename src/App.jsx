f240f1ca94c23c4a06e1c8070631be4e6e0e5108
naereloire <naere.loire@gmail.com>
2020-09-09 20:30:04 -0300

    add teste de requi

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Home from './pages/home/home';
-import Login from './pages/login/login';
-import Register from './pages/register/register';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/home/home.jsx';
+import Login from './pages/login/login.jsx';
+import Register from './pages/register/register.jsx';
 
 const App = () => {
   return (
