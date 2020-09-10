21869b363a089bbc183b61f090576c085036cc92
naereloire <naere.loire@gmail.com>
2020-09-10 13:45:16 -0300

    dev inicial obj request

--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -1,5 +1,40 @@
 import React, { useState } from 'react';
 import Form from '../../components/Form.js';
+import { v4 as uid } from 'uuid';
+
+const bodyRegister = (userInfos) => {
+  const bodyObj = {
+    idempotencyKey: uid(),
+    properties: 'sdfwerhrebgh',
+    owner: {
+      type: 'PF',
+      name: userInfos['Nome'],
+      companyName: 'TT',
+      email: 'jon@ppi.com.br',
+      homePhone: '1131859600',
+      businessPhone: '1131859600',
+      mobilePhone: '1131859600',
+      address: {
+        street: 'Winterfell',
+        number: '529',
+        district: 'Winterfell',
+        city: 'Westeros',
+        state: 'SP',
+        zipcode: '06460080',
+      },
+      identifierDocument: {
+        document: '00270965000102',
+        type: 'CPF',
+      },
+      documents: [
+        {
+          type: 'CPF',
+          document: '00270965000102',
+        },
+      ],
+    },
+  };
+};
 
 const Register = () => {
   const [dadosUser, setDadosUser] = useState({});
@@ -14,7 +49,16 @@ const Register = () => {
       <h2>CADASTRO</h2>
 
       <Form
-        options={['Nome', 'E-mail', 'CPF']}
+        options={[
+          'Nome',
+          'E-mail',
+          'CPF',
+          'Endereço',
+          'Numero',
+          'Cidade',
+          'Estado',
+          'CEP',
+        ]}
         value={dadosUser}
         setValue={setDadosUser}
       />
