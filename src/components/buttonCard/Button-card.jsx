acf716c0051afa5d135cd9b2796355489d0ab1f9
naereloire <naere.loire@gmail.com>
2020-09-11 01:12:50 -0300

    dev inicial de estilo

--- src/components/buttonCard/Button-card.jsx
+++ src/components/buttonCard/Button-card.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
+import { StyleBtn } from '../styledComponents/styledComponents';
 
 const ButtonCard = (props) => {
-    const { handleCLick, value, subtitle, img } = props;
+  const { handleCLick, value, subtitle, img } = props;
 
-    return (
-        <button onClick={handleCLick} value={value}>
-            <img src={img} alt="" />
-            <div>{subtitle}</div>
-        </button>
-    );
-}
+  return (
+    <StyleBtn onClick={handleCLick} value={value}>
+      <img src={img} alt="" />
+      <div>{subtitle}</div>
+    </StyleBtn>
+  );
+};
 export default ButtonCard;
-
