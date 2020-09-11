0cefe9df901a7256140a36a7424d94eaad6a5893
karinavit <ka.vitangelo@gmail.com>
2020-09-11 11:33:03 -0300

    iniciada a estilização da página de educação financeira

--- src/pages/finance/finance.jsx
+++ src/pages/finance/finance.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Popup from "reactjs-popup";
+import Header from '../../components/header/Header.jsx';
+import Button from '../../components/button/Button.jsx';
 import "./finance.css";
+import "../../components/styledComponents/styledComponents.jsx";
 
 const Finance = () => {
   return (
     <>
-      <h2>Concetudos de Educação Financeira</h2>
-      <Popup trigger={<button> 4 passos pra autônomos se organizarem e sobrar dinheiro </button>} position="right center">
+     <Header/>
+
+      <h2>Conteúdos de Educação Financeira</h2>
+      <Popup trigger={<Button 
+      subtitle="4 passos pra autônomos se organizarem e sobrar dinheiro">
+        
+      </Button>} position="right center">
         <div className=".popup-content"> 
           <ul>
             <li>1. Ganhe um salário</li>
